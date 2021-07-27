99bddba7f537af791e8c699bb73c0dfcc35b5f38
derick <derickaflorian@gmail.com>
2021-07-26 22:28:48 -0400

    created applyToAppropriateOperand function so logic for determining which is the current operator can be reused

--- app/src/main/java/com/example/calculator/model/CalculatorViewModel.kt
+++ app/src/main/java/com/example/calculator/model/CalculatorViewModel.kt
@@ -9,6 +9,8 @@ import kotlin.math.log10
 
 private const val FIRST_OPERAND = 1
 private const val SECOND_OPERAND = 2
+private const val FIRST_OPERAND_WITH_DECIMAL_MODE = 3
+private const val SECOND_OPERAND_WITH_DECIMAL_MODE = 4
 private const val TEN = 10
 private const val ZERO_LONG: Long = 0
 
@@ -49,28 +51,14 @@ class CalculatorViewModel : ViewModel() {
      * Add the new button press on to the end of the appropriate either the whole or fractional part
      * of the appropriate operand
      */
-    fun setOrExtendOperand(buttonPress: Int) {
-        when (getCurrentOperand()) {
-            FIRST_OPERAND -> {
-                if (_decimalModeFirstOperand) {
-                    _fractionalFirstOperand =
-                        setValueIfZeroOtherwiseExtend(_fractionalFirstOperand, buttonPress)
-                } else {
-                    _wholeFirstOperand =
-                        setValueIfZeroOtherwiseExtend(_wholeFirstOperand, buttonPress)
-                }
-            }
-            else -> {
-                if (_decimalModeSecondOperand) {
-                    _fractionalSecondOperand =
-                        setValueIfZeroOtherwiseExtend(_fractionalFirstOperand, buttonPress)
-                } else {
-                    _wholesSecondOperand =
-                        setValueIfZeroOtherwiseExtend(_wholesSecondOperand, buttonPress)
-                }
-            }
-        }
-        updateDisplay()
+    fun setOrExtendOperand(buttonPress: Int) =
+        applyToAppropriateOperand(buttonPress, this::setValueIfZeroOtherwiseExtend)
+
+    fun deleteLastEntry() =
+        applyToAppropriateOperand(this::backspace)
+
+    private fun backspace(originalValue: Long): Long {
+        return originalValue / TEN
     }
 
     /*
@@ -79,7 +67,6 @@ class CalculatorViewModel : ViewModel() {
      */
     private fun extendByOneTensPlace(originalValue: Long): Long =
         TEN * originalValue
-
 
     private fun getCurrentOperand(): Int {
         return when (_mathOperation) {
@@ -130,5 +117,53 @@ class CalculatorViewModel : ViewModel() {
     private fun Long.length() = when (this) {
         ZERO_LONG -> 1
         else -> log10(abs(toDouble())).toInt() + 1
+    }
+
+    private fun applyToAppropriateOperand(buttonPress: Int, changeOperand: (Long, Int) -> Long) {
+        when (getCurrentOperand()) {
+            FIRST_OPERAND -> {
+                if (_decimalModeFirstOperand) {
+                    _fractionalFirstOperand =
+                        changeOperand(_fractionalFirstOperand, buttonPress)
+                } else {
+                    _wholeFirstOperand =
+                        changeOperand(_wholeFirstOperand, buttonPress)
+                }
+            }
+            else -> {
+                if (_decimalModeSecondOperand) {
+                    _fractionalSecondOperand =
+                        changeOperand(_fractionalFirstOperand, buttonPress)
+                } else {
+                    _wholesSecondOperand =
+                        changeOperand(_wholesSecondOperand, buttonPress)
+                }
+            }
+        }
+        updateDisplay()
+    }
+
+    private fun applyToAppropriateOperand(changeOperand: (Long) -> Long) {
+        when (getCurrentOperand()) {
+            FIRST_OPERAND -> {
+                if (_decimalModeFirstOperand) {
+                    _fractionalFirstOperand =
+                        changeOperand(_fractionalFirstOperand)
+                } else {
+                    _wholeFirstOperand =
+                        changeOperand(_wholeFirstOperand)
+                }
+            }
+            else -> {
+                if (_decimalModeSecondOperand) {
+                    _fractionalSecondOperand =
+                        changeOperand(_fractionalFirstOperand)
+                } else {
+                    _wholesSecondOperand =
+                        changeOperand(_wholesSecondOperand)
+                }
+            }
+        }
+        updateDisplay()
     }
 }
